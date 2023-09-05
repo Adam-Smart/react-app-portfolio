6bba8d2193155044bff7ae5f6597d05844ff9b79
Adam-Smart <adams8632@hotmail.co.uk>
2023-09-06 00:29:01 +0100

    fixed padding

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function Resume() {
   return (
-    <div style ={{paddingBottom:"19%"}}>
+    <div style ={{paddingBottom:"30%"}}>
       <h1>Resume Page</h1>
       <a href="">Resume</a>
     </div>
