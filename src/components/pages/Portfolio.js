2b8625752e6f76d58d06909abac638a7780782dc
Adam-Smart <adams8632@hotmail.co.uk>
2023-09-06 00:15:50 +0100

    added repos

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -18,31 +18,37 @@ export default function Portfolio() {
       <section className='portfolio'>
         <h2>Horiseon</h2>
         <a href="https://adam-smart.github.io/week-1-code-refactor-challenge/"><img src={week1}></img></a>
+        <a href="https://github.com/Adam-Smart/week-1-code-refactor-challenge">Repositiory</a>
       </section>
       
       <section className='portfolio'>
         <h2>Chapter Chatter</h2>
         <a href="https://chapter-chatter-project-032e4d6a934f.herokuapp.com/"><img src={ChapterChatter}></img></a>
+        <a href="https://github.com/33hollie33/chapterchatter">Repositiory</a>
       </section>
 
       <section className='portfolio'>
         <h2>Note-Taker</h2>
         <a href="https://adams-note-taker-4e67211b2e7f.herokuapp.com"><img src={Note}></img></a>
+        <a href="https://github.com/Adam-Smart/Express.js-Note-Taker">Repositiory</a>
       </section>
 
       <section className='portfolio'>
         <h2>Weather Dashboard</h2>
         <a href="https://adam-smart.github.io/week-6-server-side-API-weather-dashboard/"><img src={Weather}></img></a>
+        <a href="https://github.com/Adam-Smart/week-6-server-side-API-weather-dashboard">Repositiory</a>
       </section>
 
       <section className='portfolio'>
         <h2>J.A.T.E</h2>
         <a href="https://adams-jate-text-editor-e48edddb6914.herokuapp.com/"><img src={Jate}></img></a>
+        <a href="https://github.com/Adam-Smart/pwa-text-editor">Repositiory</a>
       </section>
 
       <section className='portfolio'>
         <h2>Work day Scheduler</h2>
         <a href="https://adam-smart.github.io/week-5-work-day-scheduler/"><img src={Scheduler}></img></a>
+        <a href="https://github.com/Adam-Smart/week-5-work-day-scheduler">Repositiory</a>
       </section>
 
       
