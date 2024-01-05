e1dfb8e8119457ed16c3822d22d2a9e10b90edfe
Adam-Smart <adams8632@hotmail.co.uk>
2024-01-05 02:18:54 +0000

    edits to about me page and added link to resume.

--- src/components/pages/AboutMe.js
+++ src/components/pages/AboutMe.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Me from "../assets/PortfolioPic.jpg"
+import CV from "../../Files/Resume.pdf"
 
 
 
@@ -7,14 +8,26 @@ export default function AboutMe() {
   return (
     <div>
       <h1>About Me Page</h1>
-      <section className='profile'>
+      
+      <div className='name'>
+      <p>Hey I'm Adam Smart  </p>
+      <a href={CV} download={CV}>
+        Resume
+      </a>
+      <p>Click to Download</p>
+    </div>
+
+      <section className='container'>
+
         <img className = "profile" src={Me} alt="Adam Smart"></img>
-      <p>
+
+      <p className='info'> 
       Hello my name is Adam Smart. I am 24 years old I am currently learning how to code but have preiviously studied business studies, media studies, IT, and applied science in sixthform. 
       For the past six years i have been working ASDA where ive learned many new skills and have had to adapt to changes that they have made by them improving there inventory management.
       I have a few different hobbies, one of them being gaming. I enjoy many differnt game genres but the main i play are FPS and Simulators.
       Another hobby of mine is snowboarding. I started snowboarding about 7 years ago and love going to the mountains.
       </p>
+
       </section>
     </div>
     
