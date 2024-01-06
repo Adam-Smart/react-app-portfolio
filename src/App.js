666e4ba7db2ad05bcbb3a6abc77ea58080e15d6f
Adam-Smart <adams8632@hotmail.co.uk>
2024-01-06 23:45:41 +0000

    Changed links to social media to icons

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import './App.css';
 import React from "react";
 import PortfolioContainer from "./components/PortfolioContainer";
+import { SocialIcon } from 'react-social-icons'
 
 function App() {
     return (
       <div className="App">
         <header className="App-header"></header>
+
         <PortfolioContainer />
-        <footer>
-          <p><a href ="https://github.com/Adam-Smart">Github</a></p>
+        <body></body>
+
+        <footer className='footer-pin'>
+          <SocialIcon className="custom-class" target="_blank" url="https://www.linkedin.com/in/adam-smart-4ab57025a" />
+          <SocialIcon className="custom-class" target="_blank" url="https://github.com/Adam-Smart" />
+          <SocialIcon className="custom-class" target="_blank" url="https://x.com/Adam_Smart_98" />
+          {/* <p><a href ="https://github.com/Adam-Smart">Github</a></p>
           <p><a href="https://www.linkedin.com/in/adam-smart-4ab57025a/">LinkedIn</a></p>
-          <a href="https://twitter.com/Adam_Smart_98">X</a>
+          <a href="https://twitter.com/Adam_Smart_98">X</a> */}
 
         </footer>
       </div>
